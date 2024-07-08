21b3f2049a5ce643a6e8ff547720fb509977d17a
Mhel Edison S. Hatchazo <edisonhatchazo17@gmail.com>
2024-07-09 01:55:44 +0800

    Added Routing Solutions for the Map

--- app/src/main/java/com/example/classschedule/ui/screen/Screen.kt
+++ app/src/main/java/com/example/classschedule/ui/screen/Screen.kt
@@ -5,7 +5,6 @@ import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Screen(val route: String, val title: String, val icon: ImageVector) {
